0a6cf15688bdcfcc6f43c076136b671a7f2f1a34
najira <najira_daniela@hotmail.com>
2024-05-29 00:07:29 -0400

    routes

---
+++ routes/repertorio.route.js
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import { cancionesController, repertorioController } from '../controllers/repertorio.controller.js'
+const router = Router()
+
+router.get('/canciones', cancionesController.cancionesGet)
+router.post('/cancion', cancionesController.cancionesPost)
+router.put('/cancion/:id', cancionesController.cancionesPut)
+router.delete('/cancion', cancionesController.cancionesRemove)
+
+export default router
