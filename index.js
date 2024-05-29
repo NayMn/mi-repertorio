b64f0a491c393ff66dc66b575fc9f29542f38150
najira <najira_daniela@hotmail.com>
2024-05-28 23:02:04 -0400

    base de datos conectada

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 import express from 'express'
 import 'dotenv/config'
-
 const app = express()
+
+// leer req.body
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+// archivos estaticos
+const __dirname = import.meta.dirname
+app.use(express.static(__dirname + '/public'))
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
