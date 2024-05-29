99f14edfee1763e1984baf049598ddc904d2b798
najira <najira_daniela@hotmail.com>
2024-05-28 23:27:47 -0400

    models

---
+++ models/repertorio.model.js
@@ -0,0 +1,44 @@
+import { pool } from '../database/connection.js'
+
+const agregar = async () => {
+    const { rows } = await pool.query("SELECT * FROM canciones")
+    return rows
+}
+
+const crear = async ({ titulo, artista, tono }) => {
+    const querySql = {
+        text: "INSERT INTO CANCIONES (titulo, artista, tono) VALUES ($1, $2, $3) RETURNING *",
+        values: [titulo, artista, tono]
+    }
+    const { rows } = await pool.query(querySql)
+    return rows[0]
+}
+
+
+
+const actualizar = async ({ titulo, artista, tono, id }) => {
+    // consultas parametrizadas
+    const querySQL = {
+        text: "UPDATE canciones SET titulo = $1, artista = $2, tono = $3 WHERE id = $4 RETURNING *;",
+        values: [titulo, artista, tono, id]
+    }
+    const { rows } = await pool.query(querySQL)
+    return rows[0]
+}
+
+const eliminar = async (id) => {
+    // consultas parametrizadas
+    const querySQL = {
+        text: "DELETE FROM canciones WHERE id = $1 RETURNING *;",
+        values: [id]
+    }
+    const { rows } = await pool.query(querySQL)
+    return rows[0]
+}
+
+export const canciones = {
+    agregar,
+    crear,
+    actualizar,
+    eliminar
+}
